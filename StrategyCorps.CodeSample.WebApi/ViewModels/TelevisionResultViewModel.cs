a8ad70e4dceb987e843845767c869900a1d2e094
Martin Van Tassel <martin.vantassel@strategycorps.com>
2019-05-23 10:05:42 -0500

    Added summaries to properties to fix warnings

--- StrategyCorps.CodeSample.WebApi/ViewModels/TelevisionResultViewModel.cs
+++ StrategyCorps.CodeSample.WebApi/ViewModels/TelevisionResultViewModel.cs
@@ -1,23 +1,53 @@
 ﻿namespace StrategyCorps.CodeSample.WebApi.ViewModels
 {
+    /// <summary>
+    ///  The result contained in the response of the television search request
+    /// </summary>
     public class TelevisionResultViewModel
     {
+        /// <summary>
+        /// The popularity of the television show
+        /// </summary>
         public decimal Popularity { get; set; }
 
+        /// <summary>
+        /// The unique identifier of the television show
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// The average vote for the television show
+        /// </summary>
         public decimal VoteAverage { get; set; }
 
+        /// <summary>
+        /// The overview of the television show
+        /// </summary>
         public string Overview { get; set; }
 
+        /// <summary>
+        /// The first air date of the television show
+        /// </summary>
         public string FirstAirDate { get; set; }
         
+        /// <summary>
+        /// The original language of the television show
+        /// </summary>
         public string OriginalLanguage { get; set; }
 
+        /// <summary>
+        /// The number of votes for the television show
+        /// </summary>
         public int VoteCount { get; set; }
 
+        /// <summary>
+        /// The name of the television show
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// The original name of the television show
+        /// </summary>
         public string OriginalName { get; set; }
     }
 }
--- StrategyCorps.CodeSample.WebApi/ViewModels/TelevisionSearchResponseViewModel.cs
+++ StrategyCorps.CodeSample.WebApi/ViewModels/TelevisionSearchResponseViewModel.cs
@@ -2,14 +2,30 @@
 
 namespace StrategyCorps.CodeSample.WebApi.ViewModels
 {
+    /// <summary>
+    /// The response from the television search request
+    /// </summary>
+    // ReSharper disable once ClassNeverInstantiated.Global
     public class TelevisionSearchResponseViewModel
     {
+        /// <summary>
+        /// The results of the television search request
+        /// </summary>
         public IList<TelevisionResultViewModel> Results { get; set; }
 
+        /// <summary>
+        /// The page number of the results
+        /// </summary>
         public int Page { get; set; }
 
+        /// <summary>
+        /// The total number of results
+        /// </summary>
         public int TotalResults { get; set; }
 
+        /// <summary>
+        /// The total number of pages
+        /// </summary>
         public int TotalPages { get; set; }
     }
 }
